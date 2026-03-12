ea606c6368a6f4e723f341e2997499ec185a1a77
ScarFace <nitingupta4592@gmail.com>
2026-03-12 18:50:24 +0530

    Day2

---
+++ Array/String/highest_altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        int altitude = 0;
+        int maxAltitude = 0;
+        for(int i=0;i<n;i++){
+            altitude +=  gain[i];
+            maxAltitude = max(maxAltitude, altitude);
+        }
+        return maxAltitude;
+    }
+};

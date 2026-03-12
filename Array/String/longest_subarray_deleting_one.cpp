ad5ec5a6b677ee5e0f0deb3e46bdd8891c684c6e
Nitin Gupta <2023041240@mmmut.ac.in>
2026-03-12 20:09:59 +0530

    Update longest_subarray_deleting_one.cpp

--- Array/String/longest_subarray_deleting_one.cpp
+++ Array/String/longest_subarray_deleting_one.cpp
@@ -8,21 +8,15 @@ public:
         int maxLen = 0;
 
         for(int right = 0; right < n; right++){
-
             if(nums[right] == 0){
                 zeros++;
-            }
-
-            while(zeros > 1){
+            } while(zeros > 1){
                 if(nums[left] == 0){
                     zeros--;
                 }
                 left++;
-            }
-
-            maxLen = max(maxLen, right - left);
+            } maxLen = max(maxLen, right - left);
         }
-
         return maxLen;
     }
 };

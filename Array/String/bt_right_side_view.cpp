65f22249cb1c4de9c2ccca59a4d1bc0b0e977ccb
ScarFace <nitingupta4592@gmail.com>
2026-03-25 23:58:34 +0530

    changed the var name

--- Array/String/bt_right_side_view.cpp
+++ Array/String/bt_right_side_view.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         if(!root) return {};
-
         vector<int>result;
         queue<TreeNode*> q;
         q.push(root);
 
         while (!q.empty()) {
-            int levelSize = q.size();
+            int level_ize = q.size();
 
-            for (int i = 0; i < levelSize; i++) {
+            for (int i = 0; i < level_size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
 
-                if (i == levelSize - 1)
+                if (i == level_size - 1)
                     result.push_back(node->val);
 
                 if (node->left)  q.push(node->left);
